e11d9cac0e6162eebc6ffca76590f67f8735ef84
Carloslecval <carloslecval03@gmail.com>
2022-02-14 21:34:31 -0600

    método de gauss

--- gauss.py
+++ gauss.py
@@ -1,19 +1,27 @@
+from unittest import result
+
+
 unknowns = int(input("Number of unknowns: "))
+res = [0] * unknowns
 matrix = []
 for i in range(unknowns):
     matrix.append([0] * (unknowns + 1))
 
-def imprimirEcuacion(x = -1, y = -1):
+
+def imprimirEcuacion(x=-1, y=-1):
     placeholder = "[]"
     for i in range(unknowns):
         letter = 120
         for j in range(unknowns):
-            print(f"{placeholder if (i == x and j == y) else str(matrix[i][j])}{chr(letter)}", end=" ")
+            print(
+                f"{placeholder if (i == x and j == y) else str(matrix[i][j])}{chr(letter)}", end=" ")
             letter = (letter + 1) if letter < 122 else 97
             if j < unknowns - 1:
                 print("+ ", end="")
-        print(f"= {placeholder if (i == x and unknowns == y) else str(matrix[i][unknowns])}")
+        print(
+            f"= {placeholder if (i == x and unknowns == y) else str(matrix[i][unknowns])}")
     print()
+
 
 def one(row, column):
     if matrix[row][column] != 0:
@@ -22,13 +30,21 @@ def one(row, column):
             matrix[row][i] /= operation
     # imprimirEcuacion()
 
+
 def zeros(row, column):
-    for i in range(unknowns):
-        if i != row:
-            operation = matrix[i][column] * -1
-            for j in range(unknowns+1):
-                matrix[i][j] = matrix[i][j] + (operation * matrix[row][j])
+    for i in range(row+1 , unknowns):
+        operation = matrix[i][column] * -1
+        for j in range(unknowns+1):
+            matrix[i][j] = matrix[i][j] + (operation * matrix[row][j])
     # imprimirEcuacion()
+
+def resultado():
+    for i in reversed(range(unknowns)):
+        res[i] = matrix[i][unknowns]
+        for j in range(unknowns):
+            if j != i:
+                res[i] -= matrix[i][j] * res[j]
+    print(res)
 
 for i in range(unknowns):
     for j in range(unknowns + 1):
@@ -36,7 +52,7 @@ for i in range(unknowns):
         matrix[i][j] = int(input("Enter the value of the unknown: "))
 print()
 imprimirEcuacion()
-for i in range (unknowns):
+for i in range(unknowns):
     one(i, i)
     zeros(i, i)
-imprimirEcuacion()
+resultado()
