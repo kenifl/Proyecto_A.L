918c78e7cc95456ece5ef3b57e169cfe7ebda22d
eUlisesBaezGt <0241823@up.edu.mx>
2022-02-19 21:35:18 +0000

    Producto escalar

--- ProductoEscalar.py
+++ ProductoEscalar.py
@@ -1,7 +1,11 @@
 # Producto escalar
+k=float(input("Ingrese el número por el cuál desea multiplicar: "))
 
-f = int(input ("Introduzca el número de filas de sus matrices: "))
-c = int(input ("Introduzca el número de columnas de sus matrices: "))
+print ("\nMatriz")
+print("----------------------------\n")
+f = int(input ("Introduzca el número de filas de su matriz: "))
+c = int(input ("Introduzca el número de columnas de su matriz: "))
+print()
 
 m1 = []
 mres = []
@@ -9,22 +13,13 @@ for i in range (f):
 	m1.append( [0] * c)
 	mres.append( [0] * c)
 
-print ("\nMatriz")
-print("----------------------------\n")
-
 for i in range(f):
 		for j in range(c):
-			mres[i][j] = int(input("Posición (" + str(i+1) + "," + str(j+1) + "): "))
-
-k=float(input ("Ingrese el número por el cuál desea multiplicar: "))
+			mres[i][j] = float(input("Posición (" + str(i+1) + "," + str(j+1) + "): "))
 
 for i in range(f):
 	for j in range(c):
-            mres[i][j] += m1[i][j]
-
-for i in range(f):
-	for j in range(c):
-            mres[i][j] = mres[i][j] * k
+            mres[i][j] *= k
 
 print ("\nResultado")
 print("----------------------------")
