d5dac01a479463cda8426c7c8eea2dd7e72f94a4
Carloslecval <carloslecval03@gmail.com>
2022-04-27 18:38:05 -0500

    scalar product in js

---
+++ scalarProduct.js
@@ -0,0 +1,18 @@
+const { fraction, add, multiply, divide, equal } = require('mathjs');
+
+var k = 3;
+var f = 2;
+var c = 2;
+
+var matrix = Array(f);
+matrix1.fill(Array(c).fill(0));
+
+//aquí irían asignaciones
+
+for(var i = 0; i < f; i++)
+{
+    for(var j = 0; j < c; j++)
+    {
+        matrix[i][j] = multiply(matrix[i][j], k);
+    }
+}
