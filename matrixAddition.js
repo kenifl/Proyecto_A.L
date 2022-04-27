cc185ee7280a253c25df02c7047fcae750f37320
Carloslecval <carloslecval03@gmail.com>
2022-04-27 18:35:15 -0500

    matrix addition in js

---
+++ matrixAddition.js
@@ -0,0 +1,20 @@
+const { fraction, add, multiply, divide, equal } = require('mathjs');
+
+var rows = 3;
+var columns = 2;
+var matrix1 = Array(rows);
+matrix1.fill(Array(columns).fill(0));
+var matrix2 = Array(rows);
+matrix2.fill(Array(columns).fill(0));
+var res = Array(rows);
+res.fill(Array(columns).fill(0));
+
+//Aquí irían asignaciones
+
+for(var i = 0; i < rows; i++)
+{
+    for(var j = 0; j < columns; j++)
+    {
+        res[i][j] = add(matrix1[i][j], matrix2[i][j]);
+    }
+}
