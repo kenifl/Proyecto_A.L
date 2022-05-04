6f682569468d57517d54f9e43866802742eb91c1
kenifl <keniafl.alva@gmail.com>
2022-05-04 08:13:51 -0500

    PyQt6

--- gui.py
+++ gui.py
@@ -1,14 +1,14 @@
 # USE PYQT and create a 3z2 grid of buttons
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import pyqtSlot
 
 # create 6 buttons
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 grid layout'
+        self.title = 'PyQt6 grid layout'
         self.left = 10
         self.top = 10
         self.width = 320
@@ -57,4 +57,4 @@ class App(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
