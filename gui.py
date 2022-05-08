d6842cbe9f598e386f6dc9f856bf31460055ea9b
kenifl <keniafl.alva@gmail.com>
2022-05-07 21:25:50 -0500

    cambios

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ import sys
 from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtGui import QIcon, QFont, QPixmap, QPalette, QColor, QScreen
 from PyQt6.QtWidgets import *
+from determinant_inverse_transpose import determinant
 from inverse_gui import Inverse_GUI
 from fractions import Fraction
 
@@ -154,75 +155,43 @@ class Determinante(QMainWindow):
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
 
-    def initUI(self):
+    def create_layout(self):
         self.wid = QWidget()
-        # create grid layout
-        grid = QGridLayout()
+        b_grid = QGridLayout()
+        grid=QGridLayout()
+        r_grid=QGridLayout()
+        layout_matriz_entrada=QVBoxLayout()
+        layout_procedimiento= QVBoxLayout()
+        b_grid.addLayout(layout_matriz_entrada,0,0)
+        b_grid.addLayout(layout_procedimiento,0,1)
 
-        self.wid.setLayout(grid)
         # create labels
-        label_1 = QLabel('|A|=', self)
+        label_1=QLabel("Matriz")
+        layout_matriz_entrada.addWidget(label_1)
+        determinante= determinant()
+        layout_matriz_entrada.addLayout(grid)
+
+        label_2= QLabel('|A|=', self)
+        layout_procedimiento.addWidget(label_2)
+        for j in range(len(determinante)):
+            for i in range (len(determinante[j])):
+                label=QLabel(str(determinante[j][i]))
+                r_grid.addWidget(label, i, j)
+        layout_procedimiento.addLayout(r_grid)
+
 
         # add labels to grid
         grid.addWidget(label_1, 0, 0)
 
         # create buttons
         button_1 = QPushButton('Calcular', self)
-        button_2 = QPushButton('Regresar', self)
-        button_2.clicked.connect(self.regresar)
 
         # add buttons to grid
         grid.addWidget(button_1, 1, 1)
-        grid.addWidget(button_2, 1, 3)
+    
+        self.wid.setLayout(b_grid)
 
-    def regresar (self):
-        self.mainWindow = Principal(self.id)
-        self.mainWindow.show()
-        self.close()
 
-    def calcular(self):
-        self.mainWindow= 
-        self.mainWindow.show()
-
-        
-class Producto_escalar(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Determinante de una matriz')
-        self.centerWindow() 
-        self.initUI()
-        self.setCentralWidget(self.wid)
-
-    def centerWindow(self):
-        qtRectangle = self.frameGeometry()
-        centerPoint = self.screen().availableGeometry().center()
-        qtRectangle.moveCenter(centerPoint)
-        self.move(qtRectangle.topLeft())
-
-    def initUI(self):
-        self.wid = QWidget()
-        # create grid layout
-        grid = QGridLayout()
-
-        self.wid.setLayout(grid)
-        # create labels
-        label_1 = QLabel('|A|=', self)
-
-        # add labels to grid
-        grid.addWidget(label_1, 0, 0)
-
-        # create buttons
-        button_1 = QPushButton('Calcular', self)
-        button_2 = QPushButton('Regresar', self)
-        button_2.clicked.connect(self.regresar)
-
-        # add buttons to grid
-        grid.addWidget(button_1, 1, 1)
-        grid.addWidget(button_2, 1, 3)
-
-    def regresar (self):
-        window.show()
-        self.close()
 
 app = QApplication(sys.argv)
 window = Principal()
